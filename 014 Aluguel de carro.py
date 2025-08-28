13379494a306d2bed1b4712d60f55495103d9673
xavesplayer <Jeffersonpsalvador@hotmail.com>
2025-08-28 08:36:03 -0300

    Add files via upload
    App básico para gerenciamento de aluguel de carros

---
+++ 014 Aluguel de carro.py
@@ -0,0 +1,6 @@
+#014 Aluguel de carro
+d1 = 60
+km1 = 0.15
+d2 = int(input("Quantos dias alugados? "))
+km2 = float(input("Quantos km rodados? "))
+print (f"O total a pagar é de: {d1 * d2 + km1 * km2}")
