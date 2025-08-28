22d539eb1722ed52df362e6b35eac2cf6dcc86c6
xavesplayer <Jeffersonpsalvador@hotmail.com>
2025-08-28 08:40:02 -0300

    jogo interativo de Jokenpô

---
+++ 044 Jokenpô.py
@@ -0,0 +1,25 @@
+#044 Jokenpô
+from random import randint
+from time import sleep
+lista = ('pedra', 'papel', 'tesoura')
+print ('''Escolha pelo número: 
+1) Pedra 
+2) Papel 
+3) Tesoura ''')
+user = int(input('Digite o número: '))
+userc = lista[user - 1]
+pc = lista[randint(0, 2)]
+print('\033[33mJO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!\033[m')
+sleep(1)
+if userc == pc:
+    print('Empatamos, somos abestados!')
+elif(userc == 'pedra' and pc == 'tesoura') or \
+    (userc == 'tesoura' and pc == 'papel') or \
+    (userc == 'papel' and pc == 'pedra'):
+        print(f'Você ganhou abestado! Eu escolhi {pc}')
+else:
+    print(f'Eu ganhei! escolhi {pc}')
