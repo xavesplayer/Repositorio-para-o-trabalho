822350675219fe7bda6f20b963b28e37be14777c
xavesplayer <Jeffersonpsalvador@hotmail.com>
2025-09-04 09:11:40 -0300

    Update 044 Jokenpô.py
    abestado k

--- 044 Jokenpô.py
+++ 044 Jokenpô.py
@@ -20,6 +20,7 @@ if userc == pc:
 elif(userc == 'pedra' and pc == 'tesoura') or \
     (userc == 'tesoura' and pc == 'papel') or \
     (userc == 'papel' and pc == 'pedra'):
-        print(f'Você ganhou abestado! Eu escolhi {pc}')
+        print(f'Você ganhou abestado! Eu escolhi {pc}, abestado :D')
 else:
     print(f'Eu ganhei! escolhi {pc}')
+
